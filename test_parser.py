45e47ecc4a40c82962f6e27266dc2a6081fc32bf
c-auri <aurichchristoph@gmail.com>
2020-10-17 21:12:16 +0200

    Add tests for invalid inputs.

--- test_parser.py
+++ test_parser.py
@@ -2,11 +2,25 @@ from calc import calc
 import unittest
 
 class invalid_inputs(unittest.TestCase):
+  def test_that_non_string_inputs_raise_type_errors(self):
+    for input in [True, 0, -1, 1.2]:
+      self.assertRaises(TypeError, calc, input)
+
   def test_that_empty_strings_raise_value_errors(self):
     self.assertRaises(ValueError, calc, "")
 
   def test_that_commas_raise_value_errors(self):
     self.assertRaises(ValueError, calc, "3,14")
+
+  def test_that_incorrect_parenthesis_raise_format_errors(self):
+    incorrect_parenthesis = [
+      "(", ")", "())", "(()",
+      "(3+4", "3+4)", "(3+4))", "((3+4)",
+      "3(+)4", "(3+)4", "(3+)4"
+    ]
+
+    for input in incorrect_parenthesis:
+      self.assertRaises(FormatError, calc, input)
 
 class single_number_inputs(unittest.TestCase):
   def test_that_single_digit_integers_return_themselves(self):
