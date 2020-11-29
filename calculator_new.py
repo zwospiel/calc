291d064bc69c772004dfbe505495d12391a18133
zwospiel <johannes.seiferth@protonmail.com>
2020-11-29 16:12:39 +0100

    Create calculator_new and define method signatures

---
+++ calculator_new.py
@@ -0,0 +1,40 @@
+def parse(formula):
+    validate(formula)
+    if(is_solvable(formula)):
+        return solve(formula)
+    start, end = find_partial_formula(parse(formula))
+    partial_result = parse(formula[start:end])
+    return replace(formula, start, end, partial_result)
+
+
+def is_solvable(formula):
+    return
+
+
+def solve(formula):
+    """Solve the formula respecting operator precedence (dot before line)."""
+    return
+
+
+def find_partial_formula(formula):
+    """Return the positions of the parens surrounding the first subformula."""
+    return
+
+
+def replace(string, start, end, replacement):
+    """
+    Replace a substring of the given string with the replacement.
+
+    Arguments:
+    string, replacement:
+        The original string and the replacement for the defined substring.
+    start, end:
+        The positions in the original that define the substring to replace.
+        Both are inclusive, that means replace("my_black_tea", 3, 7, "red") 
+        will return "my_red_tea".
+    """
+    return
+
+
+def validate(formula):
+    return
